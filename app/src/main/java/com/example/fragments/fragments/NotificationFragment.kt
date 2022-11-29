2e4143c2cc44b468afa9e3d1bd7ba6aadf1c9f8e
nikuradze <nini.nikuradze.1@btu.edu.ge>
2022-11-29 23:07:55 +0400

    i keep trying

---
+++ app/src/main/java/com/example/fragments/fragments/NotificationFragment.kt
@@ -0,0 +1,18 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class NotificationFragment: Fragment(R.layout.fragment_notification) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.textView).text = NotificationFragmentArgs.fromBundle(requireArguments()).name
+
+    }
+
+}
